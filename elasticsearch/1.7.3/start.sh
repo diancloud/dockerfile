cc79360af3be52452a52d4c9944acfda4f7ffee1
Weiping <weiping@diancloud.com>
2016-07-24 20:35:41 +0800

    +Alpine 3.4  +Elasticsearch 1.7.3

---
+++ elasticsearch/1.7.3/start.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+LOG_PATH="/logs"
+if [ ! -d "/elasticsearch/config/ik" ]; then
+	rm -rf /elasticsearch/config/*
+	cd /elasticsearch/config && cp -r  /defaults/config/* ./
+fi
+
+cd /elasticsearch/bin && ./elasticsearch >> "$LOG_PATH/elasticsearch.log"
